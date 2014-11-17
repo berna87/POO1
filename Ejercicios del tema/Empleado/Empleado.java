0d5dc2cd459840ebfb5b42f92d12538b99efdd4e
Bernabe Fernandez <bernillatorres@gmail.com>
2014-11-17 13:40:39 +0100

    modificado: Ejercicios del tema/Empleado/Empleado.java

--- Ejercicios del tema/Empleado/Empleado.java
+++ Ejercicios del tema/Empleado/Empleado.java
@@ -3,8 +3,8 @@ public class Empleado{
 	private String nombre;
 	private int horasS;  //horas trabajadas por semana
 	private double sueldoH; //sueldo bruto por hora trabajada
-	private double deduccionH; //deduccion para hacienda, cifra en euros
-	private double deduccionSS; //deduccion para la Seguridad Social, cifra en euros
+	private double deduccionH; //deduccion para hacienda por semana, cifra en euros
+	private double deduccionSS; //deduccion para la Seguridad Social por semana, cifra en euros
 	
 	public Empleado(String nom,int h,double s, double dh, double dss){
 		this.nombre=nom;
@@ -18,7 +18,7 @@ public class Empleado{
 		return this.horasS*this.sueldoH;
 	}
 	public double deduccionesTotales(){
-		return this.deduccionH+this.deduccionSS;
+		return (this.deduccionH+this.deduccionSS)/100*this.sueldoBrutoSemana();
 	}
 	
 	public double sueldoNetoSemana(){
@@ -26,7 +26,7 @@ public class Empleado{
 	}
 	@Override
 	public String toString(){
-		return "Empleado: "+this.nombre+ "\nSueldo bruto: " +this.sueldoNetoSemana()+"\nSueldo por semana: "+this.sueldoBrutoSemana()+"\nDeducciones totales: "+this.deduccionesTotales();
+		return "Empleado: "+this.nombre+ "\nSueldo bruto semanal: " +this.sueldoNetoSemana()+"\nSueldo neto semanal: "+this.sueldoBrutoSemana()+"\nDeducciones totales: "+this.deduccionesTotales();
 	}
 }
 
@@ -38,9 +38,9 @@ class TestEmpleado {
 		int h = Integer.parseInt(horas);
 		String sueldo = JOptionPane.showInputDialog("Introduce el sueldo por hora");
 		double s = Double.parseDouble(sueldo);
-		String deduccionh = JOptionPane.showInputDialog("Deduccion para hacienda");
+		String deduccionh = JOptionPane.showInputDialog("Porcentaje deduccion a hacienda");
 		double dh = Double.parseDouble(deduccionh);
-		String deduccionss = JOptionPane.showInputDialog("Deduccion para la seguridad social");
+		String deduccionss = JOptionPane.showInputDialog("Porcentaje deduccion a la seguridad social");
 		double dss = Double.parseDouble(deduccionss);
 		Empleado e1 = new Empleado(nombre,h,s,dh,dss);
 		JOptionPane.showMessageDialog(null,e1);
